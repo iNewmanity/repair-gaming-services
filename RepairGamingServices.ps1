ab3b92344cca29dfe21e399d1f19d64371d0a968
iNewmanity <janneumannnneisen@gmail.com>
2022-08-31 11:56:08 +0200

    First Commit

---
+++ RepairGamingServices.ps1
@@ -0,0 +1,17 @@
+Write-Host -ForegroundColor Yellow "Removing Gaming Services Completely"
+
+Get-AppxPackage *gamingservices* -allusers | remove-appxpackage -allusers
+
+Remove-Item -Path "HKLM:\System\CurrentControlSet\Services\GamingServices" -recurse
+
+Remove-Item -Path "HKLM:\System\CurrentControlSet\Services\GamingServicesNet" -recurse
+
+Write-Host -ForegroundColor Yellow "Repairing missing System Files"
+
+sfc /scannow
+
+code C:\Windows\Logs\CBS\CBS.log
+
+Write-Host -ForegroundColor Yellow "Reinstalling pormpt to install MS Gaming services from MS Store"
+
+start ms-windows-store://pdp/?productid=9MWPM2CQNLHN
